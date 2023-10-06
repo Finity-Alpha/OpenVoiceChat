d230380c970d81fef5b3253e365607aa90c4ba2f
Fakhir Ali <ali.fakhir33@gmail.com>
2023-10-07 00:01:49 +0500

    talk in urdu

--- translation.py
+++ translation.py
@@ -1,18 +1,36 @@
 import torch
-from seamless_communication.models.inference import Translator
+from seamless_communication.models.inference import Translator as Translator_m4t
 from utils import record
 import sounddevice as sd
 
 
 
-translator = Translator("seamlessM4T_large", vocoder_name_or_card="vocoder_36langs", device=torch.device("cuda:0"))
+class Translator:
+    def __init__(self, device):
+        self.model = Translator_m4t("seamlessM4T_medium", vocoder_name_or_card="vocoder_36langs",
+                                    device=torch.device(device))
+    @torch.no_grad()
+    def say(self, text):
+        translated_text, wav, sr = self.model.predict(text, "t2st", 'urd', src_lang='eng')
+        sd.play(wav.cpu().numpy()[0][0], sr)
+        sd.wait()
+        return translated_text
+    @torch.no_grad()
+    def listen(self):
+        audio = record(2)
+        audio = torch.tensor(audio).unsqueeze(1)
+        translated_text, _, _ = self.model.predict(audio, "s2tt", 'eng')
+        return str(translated_text)
 
-audio = record(3)
-audio = torch.tensor(audio).unsqueeze(1) #idk why but this is the shape
-translated_text, wav, sr = translator.predict(audio, "s2st", 'urd')
-sd.play(wav.cpu().numpy()[0][0], sr)
-sd.wait()
-print(translated_text)
+
+if __name__ == "__main__":
+    translator = Translator_m4t("seamlessM4T_large", vocoder_name_or_card="vocoder_36langs", device=torch.device("cuda:0"))
+    audio = record(3)
+    audio = torch.tensor(audio).unsqueeze(1) #idk why but this is the shape
+    translated_text, wav, sr = translator.predict(audio, "s2st", 'urd')
+    sd.play(wav.cpu().numpy()[0][0], sr)
+    sd.wait()
+    print(translated_text)
 
 
 
