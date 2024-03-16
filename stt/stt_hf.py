8c1c4a60df5b613862e8f64b85cde006445043f2
Fakhir Ali <ali.fakhir33@gmail.com>
2024-03-17 01:12:01 +0500

    stt hf use pipeline

--- stt/stt_hf.py
+++ stt/stt_hf.py
@@ -1,4 +1,3 @@
-# from transformers import WhisperProcessor, WhisperForConditionalGeneration
 from transformers import pipeline
 import torchaudio
 import torchaudio.functional as F
@@ -11,20 +10,22 @@ print()
 
 
 class Ear_hf(BaseEar):
-    def __init__(self, model_id='openai/whisper-base.en', device='cpu', silence_seconds=2):
+    def __init__(self, model_id='openai/whisper-base.en', device='cpu',
+                 silence_seconds=2, generate_kwargs=None):
         super().__init__(silence_seconds)
         self.pipe = pipeline('automatic-speech-recognition', model=model_id, device=device)
         self.device = device
+        self.generate_kwargs = generate_kwargs
 
     @torch.no_grad()
     def transcribe(self, audio):
-        transcription = self.pipe(audio)
+        transcription = self.pipe(audio, generate_kwargs=self.generate_kwargs)
         return transcription['text'].strip()
-
 
 
 if __name__ == "__main__":
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
     ear = Ear_hf(device=device)
 
     audio, sr = torchaudio.load('../media/abs.wav')
