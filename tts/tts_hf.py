f5f3715c54ec813e2596c31b3049b860c1574d42
Fakhir Ali <ali.fakhir33@gmail.com>
2024-03-17 00:52:31 +0500

    Use hf pipeline for tts

--- tts/tts_hf.py
+++ tts/tts_hf.py
@@ -8,24 +8,26 @@ print()
 
 
 class Mouth_hf(BaseMouth):
-    def __init__(self, model_id='kakao-enterprise/vits-vctk', speaker_id=0, device='cpu'):
+    def __init__(self, model_id='kakao-enterprise/vits-vctk', device='cpu',
+                 forward_params=None):
         self.pipe = pipeline('text-to-speech', model=model_id, device=device)
         self.device = device
-        self.speaker_id = speaker_id
-        super().__init__(sample_rate=self.pipe.model.config.sampling_rate)
+        self.forward_params = forward_params
+        super().__init__(sample_rate=self.pipe.sampling_rate)
 
     @torch.no_grad()
     def run_tts(self, text):
         # inputs = self.tokenizer(text, return_tensors="pt")
         # inputs = inputs.to(self.device)
         # output = self.model(**inputs, speaker_id=self.speaker_id).waveform[0].to('cpu')
-        output = self.pipe(text, forward_params={'speaker_id': self.speaker_id})['audio'][0]
-        return output
+        output = self.pipe(text, forward_params=self.forward_params)
+        self.sample_rate = output['sampling_rate']
+        return output['audio'][0]
 
 
 if __name__ == '__main__':
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
-    mouth = Mouth_hf(device=device, speaker_id=7)
+    mouth = Mouth_hf(model_id='kakao-enterprise/vits-vctk', device=device, forward_params={"speaker_id": 10})
 
     text = ("If there's one thing that makes me nervous about the future of self-driving cars, it's that they'll "
             "replace human drivers.\nI think there's a huge opportunity to make human-driven cars safer and more "
