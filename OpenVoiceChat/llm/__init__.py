9a9b2af5e0bd457ab00b698bce53806e62cac0ab
SalehAhmad1 <salehahmad2106@gmail.com>
2024-04-28 00:19:40 +0500

    refactoring branch

--- OpenVoiceChat/llm/__init__.py
+++ OpenVoiceChat/llm/__init__.py
@@ -1,3 +1,3 @@
 from .base import BaseChatbot as BaseChatbot
 from .llm_gpt import Chatbot_gpt as Chatbot_gpt
-from .llm_llama import Chatbot as Chatbot_llama
+from .llm_llama import Chatbot_llama as Chatbot_llama
