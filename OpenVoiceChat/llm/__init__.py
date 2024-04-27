24448517875342a701dc0c20d802ae609a06d634
SalehAhmad1 <salehahmad2106@gmail.com>
2024-04-28 00:04:01 +0500

    refactoring branch

--- OpenVoiceChat/llm/__init__.py
+++
@@ -1,4 +0,0 @@
-# from .base import *
-# from .llm_gpt import *
-# from .llm_llama import *
-# from .llm_hf import *
--- OpenVoiceChat/stt/__init__.py
+++
@@ -1,2 +0,0 @@
-# from .stt_hf import Ear as Ear_hf
-# from .stt_vosk import Ear as Ear_vosk
