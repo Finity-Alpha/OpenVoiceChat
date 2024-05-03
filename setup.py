4f9186f6236206d90d14f4910b1802bcafe02239
Fakhir Ali <ali.fakhir33@gmail.com>
2024-05-04 02:33:42 +0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,16 @@
 from setuptools import setup, find_packages
 
 setup(
+    author='Fakhir Ali',
+    author_email='ali.fakhir33@gmail.com',
+    description='OpenVoiceChat is an opensource library that allows you to have a natural voice conversation with '
+                'your LLM agent.',
+    long_description='If you plan on making an LLM agent and want to have your users be able to talk to it like a '
+                     'person (low latency, handles interruptions), this library is for you. It aims to be the '
+                     'opensource, highly extensible and easy to use alternative to the proprietary solutions.',
+    url='http://www.fakhirali.pk/OpenVoiceChat/',
     name='openvoicechat',
-    version='0.1',
+    version='0.1.2',
     packages=find_packages(),
     install_requires=[
         'sounddevice',
