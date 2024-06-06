4b90f788e432a697f15bca817a512d96dd6f06e7
Fakhir Ali <ali.fakhir33@gmail.com>
2024-06-06 17:30:17 +0500

    Revert "Time tracking"

--- main.py
+++ main.py
@@ -1,23 +1,15 @@
-from openvoicechat.tts.tts_elevenlabs import Mouth_elevenlabs as Mouth
+from openvoicechat.tts.tts_hf import Mouth_hf as Mouth
 from openvoicechat.llm.llm_gpt import Chatbot_gpt as Chatbot
 from openvoicechat.stt.stt_hf import Ear_hf as Ear
 from openvoicechat.utils import run_chat
 from openvoicechat.llm.prompts import llama_sales
 from dotenv import load_dotenv
 import os
-import pandas as pd
 
 if __name__ == "__main__":
     device = 'cpu'
 
     print('loading models... ', device)
-
-
-
-    columns = ['Response','stt', 'tts', 'llm']
-    df = pd.DataFrame(columns=columns)
-    file_path = 'timing.csv'
-    df.to_csv(file_path, index=False)
 
     ear = Ear(silence_seconds=2)
 
@@ -26,8 +18,7 @@ if __name__ == "__main__":
 
     chatbot = Chatbot(sys_prompt=llama_sales,
                       api_key=api_key)
-
-
-    g_api_key = os.getenv('ELEVENLABS_API_KEY')
-    mouth = Mouth()
+    mouth = Mouth(device=device,
+                  forward_params={"speaker_id": 10})
+    mouth.say_text('Good morning!')
     run_chat(mouth, ear, chatbot, verbose=True, stopping_criteria=lambda x: '[END]' in x)
