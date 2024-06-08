cac0a82f8f6360ff027ddb2d1c21fad15e55ef39
Fakhir Ali <ali.fakhir33@gmail.com>
2024-06-08 20:50:07 +0500

    Made llama-cpp generic chatbot

--- openvoicechat/llm/llm_llama.py
+++ openvoicechat/llm/llm_llama.py
@@ -1,40 +1,35 @@
-from .base import BaseChatbot
-
+if __name__ == '__main__':
+    from base import BaseChatbot
+else:
+    from .base import BaseChatbot
 class Chatbot_llama(BaseChatbot):
-    def __init__(self, model_path='models/llama-2-7b.Q4_K_M.gguf', device='cuda', sys_prompt='',
-                 break_words=None, name='[JOHN]'):
+    def __init__(self, model_path='models/llama-2-7b.Q4_K_M.gguf', device='cuda', sys_prompt=''):
         from llama_cpp import Llama
-        if break_words is None:
-            break_words = ['[USER]', '[END]']
         self.model = Llama(model_path=model_path, n_ctx=4096,
                            n_gpu_layers=-1 if device == 'cuda' else 0,
-                           verbose=False)
-        self.sys_prompt = sys_prompt
-        self.break_words = break_words
-        self.name = name
-        self.messages = []
-        self.messages.append(self.sys_prompt)
+                           verbose=False, chat_format='llama-2')
+        self.messages = [{'role': 'system', 'content': sys_prompt}]
 
     def run(self, input_text):
-        self.messages.append(input_text + '\n' + self.name)
-        out = self.model.create_completion(''.join(self.messages),
-                                           max_tokens=1000, stream=True)
+        self.messages.append({'role': 'user', 'content': input_text})
+        out = self.model.create_chat_completion(self.messages, stream=True)
         response_text = ''
         for o in out:
-            text = o['choices'][0]['text']
-            response_text += text
-            yield text
-            if any([response_text.strip().endswith(break_word) for break_word in self.break_words]):
+            if 'content' in o['choices'][0]['delta'].keys():
+                text = o['choices'][0]['delta']['content']
+                response_text += text
+                yield text
+            if o['choices'][0]['finish_reason'] is not None:
                 break
 
     def post_process(self, response):
-        self.messages.append(response)
+        self.messages.append({'role': 'assistant', 'content': response})
         return response
 
 
 if __name__ == "__main__":
-    preprompt = call_pre_prompt
-    john = Chatbot_llama(model_path='../models/llama-2-7b.Q4_K_M.gguf', sys_prompt=preprompt)
+    from prompts import llama_sales as preprompt
+    john = Chatbot_llama(model_path='../../models/llama-2-7b-chat.Q4_K_M.gguf', sys_prompt=preprompt)
     print("type: exit, quit or stop to end the chat")
     print("Chat started:")
     while True:
