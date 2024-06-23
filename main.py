d21c80e92af5238c3378a9331ff7beb64f838837
Fakhir Ali <ali.fakhir33@gmail.com>
2024-06-23 14:16:57 +0500

    General main

--- main.py
+++ main.py
@@ -1,51 +1,28 @@
 import os
-from openvoicechat.tts.tts_hf import Mouth_hf as Mouth
-from openvoicechat.llm.llm_gpt import Chatbot_gpt as Chatbot
-from openvoicechat.llm.base import BaseChatbot
-from openvoicechat.stt.stt_hf import Ear_hf as Ear
+from openvoicechat.tts.tts_elevenlabs import Mouth_elevenlabs
+from openvoicechat.tts.tts_piper import Mouth_piper
+from openvoicechat.llm.llm_gpt import Chatbot_gpt
+from openvoicechat.llm.llm_llama import Chatbot_llama
+from openvoicechat.stt.stt_deepgram import Ear_deepgram
+from openvoicechat.stt.stt_hf import Ear_hf
 from openvoicechat.utils import run_chat
 from openvoicechat.llm.prompts import llama_sales
-from together import Together
 from dotenv import load_dotenv
 load_dotenv()
 
-class Chatbot_together(BaseChatbot):
-    def __init__(self, sys_prompt='', Model='mistralai/Mixtral-8x7B-Instruct-v0.1'):
-        api_key = os.getenv("TOGETHER_API_KEY")
-        print(api_key)
-        self.MODEL = Model
-        self.client = Together(api_key=api_key)
-        self.messages = []
-        self.messages.append({"role": "system", "content": sys_prompt})
 
-    def run(self, input_text):
-        self.messages.append({"role": "user", "content": input_text})
-
-        stream = self.client.chat.completions.create(
-            model=self.MODEL,
-            messages=self.messages,
-            stream=True,
-        )
-
-        for chunk in stream:
-            if chunk.choices[0].delta.content is not None:
-                yield chunk.choices[0].delta.content
-
-    def post_process(self, response):
-        self.messages.append({"role": "assistant", "content": response})
-        return response
-    
 if __name__ == "__main__":
-    device = 'cpu'
+    device = 'cuda'
 
     print('loading models... ', device)
-
-    ear = Ear(silence_seconds=2)
+    api_key = os.getenv('DEEPGRAM_API_KEY')
+    # ear = Ear_deepgram(silence_seconds=2, api_key=api_key)
+    ear = Ear_hf(silence_seconds=2, device=device)
 
     load_dotenv()
 
-    chatbot = Chatbot_together(sys_prompt=llama_sales)
+    chatbot = Chatbot_llama(sys_prompt=llama_sales)
 
-    mouth = Mouth()
+    mouth = Mouth_piper(device=device)
 
     run_chat(mouth, ear, chatbot, verbose=True, stopping_criteria=lambda x: '[END]' in x)
