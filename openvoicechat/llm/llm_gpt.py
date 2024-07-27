e10b0221bf24c67bf5ff24c6e68b0e5538c9eacc
Fakhir Ali <ali.fakhir33@gmail.com>
2024-07-27 16:40:52 +0500

    fixing

--- openvoicechat/llm/llm_gpt.py
+++ openvoicechat/llm/llm_gpt.py
@@ -81,7 +81,8 @@ class Chatbot_gpt(BaseChatbot):
                             }]
                     })
                     # run the function
-                    function_response = self.functions[func_call['name']](*json.loads(func_call['arguments']))
+                    print(func_call['arguments'])
+                    function_response = self.functions[func_call['name']](**json.loads(func_call['arguments']))
                     self.messages.append({
                         "tool_call_id": func_call['id'],
                         "role": "tool",
