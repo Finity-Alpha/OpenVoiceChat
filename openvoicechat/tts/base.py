4744e4e791d5d857d6ff815a562a8d858a4c9ba7
Fakhir Ali <ali.fakhir33@gmail.com>
2024-07-31 20:21:43 +0500

    update docs

--- openvoicechat/tts/base.py
+++ openvoicechat/tts/base.py
@@ -35,27 +35,27 @@ class BaseMouth:
         self.seg = pysbd.Segmenter(language="en", clean=True)
 
     def run_tts(self, text: str) -> np.ndarray:
-        '''
+        """
         :param text: The text to synthesize speech for
         :return: audio numpy array for sounddevice
-        '''
+        """
         raise NotImplementedError('This method should be implemented by the subclass')
 
     def say_text(self, text: str):
-        '''
+        """
         :param text: The text to synthesize speech for
         calls run_tts and plays the audio using sounddevice.
-        '''
+        """
         output = self.run_tts(text)
         self.player.play(output, samplerate=self.sample_rate)
         self.player.wait()
 
     def say(self, audio_queue: queue.Queue, listen_interruption_func: Callable):
-        '''
+        """
         :param audio_queue: The queue where the audio is stored for it to be played
         :param listen_interruption_func: callable function from the ear class.
         Plays the audios in the queue using sounddevice. Stops if interruption occurred.
-        '''
+        """
         self.interrupted = ''
         while True:
             output, text = audio_queue.get()
@@ -73,12 +73,12 @@ class BaseMouth:
                 self.player.wait()
 
     def say_multiple(self, text: str, listen_interruption_func: Callable):
-        '''
+        """
         :param text: Intput text to synthesize
         :param listen_interruption_func: callable function from the ear class
-        Splits the text into sentences separated by ['.', '?', '!']. Then plays the sentences one by one
-        using run_tts and say
-        '''
+        Splits the text into sentences. Then plays the sentences one by one
+        using run_tts() and say()
+        """
         sentences = self.seg.segment(text)
         print(sentences)
         audio_queue = queue.Queue()
@@ -104,14 +104,14 @@ class BaseMouth:
                             listen_interruption_func: Callable,
                             interrupt_queue: queue.Queue,
                             audio_queue: queue.Queue = None):
-        '''
+        """
         :param text_queue: The queue where the llm adds the predicted tokens
         :param listen_interruption_func: callable function from the ear class
         :param interrupt_queue: The queue where True is put when interruption occurred.
         :param audio_queue: The queue where the audio to be played is placed
         Receives text from the text_queue. As soon as a sentence is made run_tts is called to
-        synthesize its speech.
-        '''
+        synthesize its speech and sent to the audio_queue for it to be played.
+        """
         response = ''
         all_response = []
         interrupt_text_list = []
